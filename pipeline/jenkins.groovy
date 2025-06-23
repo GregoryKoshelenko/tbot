97811b6c8bf36fd5123d00b5515e48f5ebdd0b11
Gregory Koshelenko <gregory.koshelenko@gmail.com>
2025-06-23 23:17:00 +0300

    Add stages for linting, testing, building, and pushing Docker images in Jenkins pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -22,4 +22,56 @@ pipeline {
             description: 'Skip running linter'
         )
     }
+    environment {
+        TARGETOS = "${params.OS}"
+        TARGETARCH = "${params.ARCH}"
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Lint') {
+            when { expression { !params.SKIP_LINT } }
+            steps {
+                sh 'make lint'
+            }
+        }
+        stage('Test') {
+            when { expression { !params.SKIP_TESTS } }
+            steps {
+                sh 'make test'
+            }
+        }
+        stage('Build') {
+            steps {
+                script {
+                    def target = ''
+                    if (params.OS == 'linux' && params.ARCH == 'amd64') target = 'linux'
+                    else if (params.OS == 'linux' && params.ARCH == 'arm64') target = 'arm'
+                    else if (params.OS == 'darwin' && params.ARCH == 'amd64') target = 'macos'
+                    else if (params.OS == 'darwin' && params.ARCH == 'arm64') target = 'macos-arm'
+                    else if (params.OS == 'windows' && params.ARCH == 'amd64') target = 'windows'
+                    else if (params.OS == 'windows' && params.ARCH == 'arm64') target = 'windows-arm'
+                    sh "make ${target}"
+                }
+            }
+        }
+        stage('Docker Build') {
+            steps {
+                sh 'make image'
+            }
+        }
+        stage('Push Image') {
+            steps {
+                sh 'make push'
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
 }
